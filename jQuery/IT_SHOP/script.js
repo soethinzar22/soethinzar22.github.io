3a2006f9eeec69e638202f51464162dfc2f56646
soethinzar22 <soethinzar2268@gmail.com>
2026-03-29 15:44:56 +0630

    add to cart

--- jQuery/IT_SHOP/script.js
+++ jQuery/IT_SHOP/script.js
@@ -55,4 +55,89 @@ $(document).ready(function(){
             })
         }
     }
+
+    getData();
+    function getData() {
+        let itemString = localStorage.getItem('shop');
+        if(itemString) {
+            let itemArray = JSON.parse(itemString);
+            
+            let data = '';
+            let no = 1 ;
+            let total = 0;
+            $.each(itemArray,function(i,v){
+                let name = v.name;
+                let price = v.price;
+                let qty = v.qty;
+
+                data += `<tr>
+                            <td>${no++}</td>
+                            <td>${name}</td>
+                            <td>${price}</td>
+                            <td>
+                                <button class="min" data-key="${i}"> - </button>
+                                ${qty}
+                                <button class="max" data-key="${i}"> + </button> 
+                            </td>
+                            <td>${price * qty}</td>
+                        </tr>`;
+
+                        total += price * qty;
+            })
+
+            data += `<tr>
+                        <td colspan="4" align="right">Total</td>
+                        <td>${total}</td>
+                    </tr>`;
+            $('tbody').html(data);
+        }
+    }
+
+    $('tbody').on('click','.min',function(){
+        let key = $(this).data('key');
+        
+        let itemString = localStorage.getItem('shop');
+        if(itemString) {
+            let itemArray = JSON.parse(itemString);
+            
+            $.each(itemArray,function(i,v){
+                if(key == i){
+                    v.qty--;
+                    if(v.qty == 0){
+                        let ans = confirm("Are You Sure delete?");
+                        if(ans){
+                            itemArray.splice(key,1);
+                        }else{
+                            v.qty=1;
+                        }
+                    }
+                }
+            })
+
+        let itemData = JSON.stringify(itemArray);
+        localStorage.setItem('shop',itemData);
+        getData();
+        count();
+        }
+    })
+
+    $('tbody').on('click','.max',function(){
+        let key = $(this).data('key');
+        
+        let itemString = localStorage.getItem('shop');
+        if(itemString) {
+            let itemArray = JSON.parse(itemString);
+            
+            $.each(itemArray,function(i,v){
+                if(key == i){
+                    v.qty++;
+                }
+            })
+
+        let itemData = JSON.stringify(itemArray);
+        localStorage.setItem('shop',itemData);
+        getData();
+        count();
+        }
+    })
 })
